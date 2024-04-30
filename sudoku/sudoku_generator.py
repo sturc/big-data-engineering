c6ef81ba7659f906dc06051a9662db00129d2548
Christoph Sturm <43139608+sturc@users.noreply.github.com>
2024-04-30 14:56:59 +0200

    Bug in result list fixed

--- sudoku/sudoku_generator.py
+++ sudoku/sudoku_generator.py
@@ -18,12 +18,12 @@ random.seed(1234)
 diff_levels = list(DIFFICULTY_LEVELS.keys())
 diff_levels.remove("Pretty hard")
 diff_levels.remove("Hard")
-result = list()
 # write the headers
 with open(path+file_name,'w',newline='') as out:
     csv_out=csv.writer(out,delimiter=',',quoting=csv.QUOTE_NONE, escapechar='\\')
     csv_out.writerow(['quizzes','solutions','difficulty','num_solutions'])       
 for i in range (100):
+    result = list()
     for i in range(10000):
         quiz, solution, diff_level, num_sol = Sudoku().generate(random.choice(diff_levels))
         result.append ((''.join(map(str, np.concatenate(quiz))),
